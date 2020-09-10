0e3122bb42fffa272a24c2da014c7df9bc957e9d
sharechiwai <yau.chiwai@gmail.com>
2020-09-10 21:49:34 +0800

    Added IP search box to search for IP Geo Info

--- src/pages/ipgeoinfo/IPGeoInfo.js
+++ src/pages/ipgeoinfo/IPGeoInfo.js
@@ -1,28 +1,72 @@
 import React, {useState, useEffect} from 'react';
 import {Map, Marker, Popup, TileLayer} from 'react-leaflet';
+import {Button, InputLabel, TextField} from '@material-ui/core';
 
 const IPGeoInfo = () => {
   const [activeIPInfo, setActiveIPInfo] = useState(null);
+  const [ip, setIP] = useState(22.302711);
   const [lat, setLat] = useState(22.302711);
   const [lng, setLng] = useState(114.177216);
 
   useEffect(() => {
-    fetch('http://ip-api.com/json')
+    fetch('https://api.ipify.org?format=json')
       .then((res) => res.json())
-      .then(
-        (result) => {
-          setLat(result.lat);
-          setLng(result.lon);
-          setActiveIPInfo(result);
-        },
-        (error) => {
-          console.log(error);
-        },
-      );
+      .then((result) => {
+        const ip = result.ip;
+        setIP(ip);
+        fetch(`https://powertoys.vercel.app/geoipservices/${ip}`)
+          .then((res) => res.json())
+          .then(
+            (result) => {
+              setLat(result.lat);
+              setLng(result.lon);
+              setActiveIPInfo(result);
+            },
+            (error) => {
+              console.log(error);
+            },
+          );
+      });
   }, []);
+
+  function sayHello() {
+    if (
+      /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+        ip,
+      )
+    ) {
+      fetch(`https://powertoys.vercel.app/geoipservices/${ip}`)
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            setLat(result.lat);
+            setLng(result.lon);
+            setActiveIPInfo(result);
+          },
+          (error) => {
+            console.log(error);
+          },
+        );
+    } else {
+      alert('Please enter valid IP address');
+    }
+  }
 
   return (
     <div>
+      <form noValidate autoComplete='off'>
+        <InputLabel>IP:</InputLabel>
+        <TextField
+          label='IP Address'
+          variant='outlined'
+          color='secondary'
+          value={ip}
+          onChange={(e) => setIP(e.target.value)}
+        />
+        <Button variant='contained' color='primary' onClick={sayHello}>
+          Search
+        </Button>
+      </form>
       <Map center={[lat, lng]} zoom={13}>
         <TileLayer
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
